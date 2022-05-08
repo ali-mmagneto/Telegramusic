1d3c94d1e82079d4f77459ed57ea1b55382bfdfa
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-05-08 15:13:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -450,7 +450,7 @@ async def help_start(event: types.Message):
     bot_username = bot_info.username.replace("_", "\\_").replace("*", "\\*").replace("[", "\\[").replace("`", "\\`")
     
     result = builder.article(
-        text= "Hey, Ben *{}*\n.format(bot_name)\n Beni Satır içi modda kullanabilirsin:\n Örnek: `@{} a man without love`\n".format(bot_username)\n veya sadece *Deezer* albüm ya da müzik *link* yolla \\!", 
+        text= "Hey, Ben *{}*\n.format(bot_name)\n Beni Satır içi modda kullanabilirsin:\n Örnek: `@{} a man without love`\n veya sadece *Deezer* albüm ya da müzik *link* yolla \\!", 
         parse_mode="MarkdownV2",
         buttons=[
                 [Button.switch_inline("🔎 ara", query="", same_peer=True)], 
